81d4ecaed651e328127295de0962ebde5388145d
Attacktive <tribute2pantera@gmail.com>
2025-11-30 21:31:23 +0900

    test: fix broken test by using H2

--- src/test/kotlin/xyz/attacktive/busstatistics/BusStatisticsApplicationTests.kt
+++ src/test/kotlin/xyz/attacktive/busstatistics/BusStatisticsApplicationTests.kt
@@ -2,12 +2,15 @@ package xyz.attacktive.busstatistics
 
 import kotlin.test.assertNotNull
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import xyz.attacktive.busstatistics.statistics.adapter.StatisticsController
 import xyz.attacktive.busstatistics.statistics.adapter.StatisticsService
 
 @SpringBootTest
-class BusStatisticsApplicationTests(private val statisticsController: StatisticsController, private val statisticsService: StatisticsService) {
+@ActiveProfiles("test")
+class BusStatisticsApplicationTests @Autowired constructor(private val statisticsController: StatisticsController, private val statisticsService: StatisticsService) {
 	@Test
 	fun contextLoads() {
 		assertNotNull(statisticsController)
