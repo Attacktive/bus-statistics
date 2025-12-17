8345268a716d68a748967f9a8d95a1dd89c49fa7
Attacktive <tribute2pantera@gmail.com>
2025-12-17 15:20:25 +0900

    chore: introduce local variable for Kotlin plugin versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	val kotlinGradlePluginVersion = "2.3.0"
+
+	kotlin("jvm") version kotlinGradlePluginVersion
+	kotlin("plugin.spring") version kotlinGradlePluginVersion
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
